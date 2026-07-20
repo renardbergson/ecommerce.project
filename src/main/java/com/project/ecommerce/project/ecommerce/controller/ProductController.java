5d8fb735720cfe8db20164e2d9515cb36531aca1
renardbergson <renard.contato@gmail.com>
2026-07-19 23:14:19 -0300

    feat(products): add delete feature

--- src/main/java/com/project/ecommerce/project/ecommerce/controller/ProductController.java
+++ src/main/java/com/project/ecommerce/project/ecommerce/controller/ProductController.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 import com.project.ecommerce.project.ecommerce.models.Product;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/admin")
@@ -15,8 +19,18 @@ public class ProductController {
 
     @GetMapping("/product")
     public String product(Model model) {
-        model.addAttribute("product", new Product());
-        model.addAttribute("adminSection", "product");
+        if(model.containsAttribute("product")) {
+            model.addAttribute("product", model.getAttribute("product"));
+        } else {
+            model.addAttribute("product", new Product());
+        }
+
+        if(model.containsAttribute("adminSection")) {
+            model.addAttribute("adminSection", model.getAttribute("adminSection"));
+        } else {
+            model.addAttribute("adminSection", "product");
+        }
+
         model.addAttribute("products", productRepository.findAll());
         return "admin";
     }
@@ -25,6 +39,34 @@ public class ProductController {
     public String save(@ModelAttribute Product product) {
         productRepository.save(product);
         System.out.println(product);
+        return "redirect:/admin/product";
+    }
+
+    @GetMapping("/product/edit/{id}")
+    public String edit(@PathVariable UUID id, RedirectAttributes redirectAttributes) {
+        Optional<Product> product = productRepository.findById(id);
+        if(product.isPresent()) {
+            redirectAttributes.addFlashAttribute("product", product.get());
+        } else {
+            System.out.println("Product not found! " + id);
+        }
+        return "redirect:/admin/product";
+    }
+
+    @GetMapping("/product/delete/{id}")
+    public String delete(@PathVariable UUID id, RedirectAttributes redirectAttributes) {
+        Optional<Product> product = productRepository.findById(id);
+        if(product.isPresent()) {
+            redirectAttributes.addFlashAttribute("product", product.get());
+            redirectAttributes.addFlashAttribute("adminSection", "confirm-delete-product");
+        }
+        return "redirect:/admin/product";
+    }
+
+    @GetMapping("/product/delete/{id}/confirm")
+    public String confirmDelete(@PathVariable UUID id) {
+        productRepository.deleteById(id);
+        System.out.println("Product deleted from database: " + id);
         return "redirect:/admin/product";
     }
 }
